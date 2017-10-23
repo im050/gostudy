3deaeeb9723d53c3ccc6b574a0b223056d97d261
linyl <service@im050.com>
2017-10-23 21:17:49 +0800

    update

--- src/myapp/interface.go
+++ src/myapp/interface.go
@@ -2,6 +2,34 @@ package main
 
 import "fmt"
 
-func main() {
+type Animal interface {
+    eat() string
+}
 
+type Tiger struct {
+    Name string
+}
+
+type Monkey struct {
+    Name string
+}
+
+func main() {
+    var m = new(Monkey)
+    var t = new(Tiger)
+    m.Name = "老猴子"
+    t.Name = "傻狮子"
+    m.eat()
+    t.eat()
+    //目前不清楚New和这样的方式创建对象有没有区别,2017年10月23日21:17:38
+    var sm = Monkey{Name:"小猴子"}
+    sm.eat()
+}
+
+func(t Tiger) eat() {
+    fmt.Println(t.Name, "：我吃香蕉")
+}
+
+func(m Monkey) eat() {
+    fmt.Println(m.Name, "：我吃榴莲")
 }
